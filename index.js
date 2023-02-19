be249856d4dffd0ed31f8dc17bddcef2eb99d436
enchanted-coder <kosgeidaniel5216@gmail.com>
2023-02-19 17:01:46 +0300

    added onclick html elements on all buttons added basic javascrip functions to change the innerText object onclick

---
+++ index.js
@@ -0,0 +1,38 @@
+const btnone = document.getElementById("btn-1")
+const btntwo = document.getElementById("btn-2")
+const btnthree = document.getElementById("btn-3")
+const btnfour = document.getElementById("btn-4")
+const btnfive = document.getElementById("btn-5")
+const btnsix = document.getElementById("btn-6")
+const btnseven = document.getElementById("btn-7")
+const btneight = document.getElementById("btn-8")
+const btnnine = document.getElementById("btn-9")
+
+function changeBtnOne(){
+  btnone.innerText = "clicked"
+}
+
+function changeBtnTwo(){
+  btntwo.innerText = "clicked" 
+}
+function changeBtnThree(){
+  btnthree.innerText = "clicked"
+}
+function changeBtnFour(){
+  btnfour.innerText = "clicked"
+}
+function changeBtnFive(){
+  btnfive.innerText = "clicked"
+}
+function changeBtnSix(){
+  btnsix.innerText = "clicked"
+}
+function changeBtnSeven(){
+  btnseven.innerText = "clicked"
+}
+function changeBtnEight(){
+  btneight.innerText = "clicked"
+}
+function changeBtnNine(){
+  btnnine.innerText = "clicked"
+}
