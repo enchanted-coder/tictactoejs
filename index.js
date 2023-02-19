23a95ce6e46e303cd12e229500a8c1aadbb64d4e
enchanted-coder <kosgeidaniel5216@gmail.com>
2023-02-19 14:39:55 +0300

    created our main variables for index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+const oneone = document.getElementById("oneone")
+const onetwo = document.getElementByID("onetwo")
+const onethree = document.getElementById("onethree")
+const twoone = document.getElementById("twoone")
+const twotwo = document.getElementById("twotwo")
+const twothree = document.getElementById("twothree")
+const threeone = document.getElementById("threeone")
+const threetwo = document.getElementById("threetwo")
+const threethree = document.getElementById("threethree")
+
